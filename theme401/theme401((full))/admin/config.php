5628b6944c13a937668eae56c4da67833d19012b
VoAnhHuy123 <huyvo2581999@gmail.com>
2022-10-14 16:53:12 +0000

    first

---
+++ theme401/theme401((full))/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/weeb/theme401/theme401((full))/admin/');
+define('HTTP_CATALOG', 'http://localhost/weeb/theme401/theme401((full))/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/weeb/theme401/theme401((full))/admin/');
+define('HTTPS_CATALOG', 'http://localhost/weeb/theme401/theme401((full))/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/');
+define('DIR_DATABASE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/database/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/logs/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'alan');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ theme401/theme401((full))/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/weeb/theme401/theme401((full))/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/weeb/theme401/theme401((full))/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/');
+define('DIR_DATABASE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/database/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/weeb/theme401/theme401((full))/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'alan');
+define('DB_PREFIX', 'oc_');
+?>
